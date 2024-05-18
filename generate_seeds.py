9601d68cbfe5a59974fdefe29294ee842a533552
Adrian Layer <adrian.layer@gmail.com>
2024-05-18 13:14:25 -0700

    Added seeds/best seed, rmved local/mult align

--- generate_seeds.py
+++ generate_seeds.py
@@ -1,8 +1,3 @@
-import sys
-from typing import List, Dict, Iterable, Tuple
-
-# Please do not remove package declarations because these are used by the autograder.
-
 def burrows_wheeler_transform(text: str) -> str:
     """
     Generate the Burrows-Wheeler Transform of the given text.
@@ -16,24 +11,11 @@ def burrows_wheeler_transform(text: str) -> str:
         bwt = bwt + rotation[len(rotation)-1]
     return bwt
 
-def suffix_array(text: str) -> List[int]:
-    """
-    Generate the suffix array for the given text.
-    """
-    s_array = []
-    suffixes = []
-    for i in range(0, len(text)):
-        suffixes.append(text[i:])
-    sorted_suffixes = sorted(suffixes)
-    for suffix in sorted_suffixes:
-        s_array.append(text.index(suffix))
-    return s_array
-
 def partial_suffix_array(text: str, k: int) -> dict[int, int]:
     """
     Generate a partial suffix array for the given text and interval K.
     """
-    full_suffix_array = suffix_array(text)
+    full_suffix_array = sorted(range(len(text)), key=lambda i: text[i:]) # taken from Nabil's bw code
     partial = dict()
     for idx in full_suffix_array:
         if full_suffix_array[idx] % k == 0:
@@ -90,6 +72,14 @@ def compute_checkpoint_arrs(bwt: str) -> dict[int, list[int]]:
             ranks[i] = rank[:]
     return ranks
 
+def compute_rank(bwt: str, idx: int, ranks: dict[int, list[int]], symbol: str, C: int) -> int: # idx can be either top or bot
+    idx_dist_from_chkpnt = idx % C
+    idx_rank = ranks[idx - idx_dist_from_chkpnt][ord(symbol)]
+    for j in range(idx - idx_dist_from_chkpnt + 1, idx + 1):
+        if bwt[j] == symbol:
+            idx_rank += 1
+    return idx_rank
+
 def bw_better_match_pattern(bwt: str, pattern: str, first_occurrences: dict[str,int], ranks: list[list[int]]) -> tuple[int,int]:
     C = 5
     top = 0
@@ -125,49 +115,39 @@ def compute_idxes_from_top_bot(start: int, end: int, partial_s_array: dict[int, 
         pattern_idxes.append((partial_s_array[p] + plus_count) % len(bwt))
     return pattern_idxes
 
-def compute_rank(bwt: str, idx: int, ranks: dict[int, list[int]], symbol: str, C: int) -> int: # idx can be either top or bot
-    idx_dist_from_chkpnt = idx % C
-    idx_rank = ranks[idx - idx_dist_from_chkpnt][ord(symbol)]
-    for j in range(idx - idx_dist_from_chkpnt + 1, idx + 1):
-        if bwt[j] == symbol:
-            idx_rank += 1
-    return idx_rank
-
-# Insert your multiple_pattern_matching function here, along with any subroutines you need
-def multiple_pattern_matching(text: str, patterns: List[str]) -> Dict[str, List[int]]:
+def generate_seeds(read: str, bwt: str, k: int, psa: dict[int, int],
+                                                first_occurrences: dict[str, int],
+                                                checkpoint_arrs: dict[int, list[int]],
+                                                ranks: list[int]) -> list[list[int]]:
     """
-    Find all starting positions in text where each string from patterns appears as a substring.
+    Takes a read and bwt created from reference genome, and generates a list of lists
+    with each index being an index i in the read from 0 to len(read) - k + 1. The corresponding list at each
+    index is a list of exact match indices of the kmer at read[i:i+k] located in the reference
+    genome. Note that even if two kmers are identical, their indices in the read are not.
     """
-    text = text + '$'
-    pattern_indices = dict()
-    bwt = burrows_wheeler_transform(text)
-    first_occurrences = compute_first_occurrences(bwt)
-    ranks = compute_checkpoint_arrs(bwt)
-    rank = compute_rank_arr(bwt)
-    K = 5
-    partial_s_array = partial_suffix_array(text, K)
-    for pattern in patterns:
-        start, end = bw_better_match_pattern(bwt, pattern, first_occurrences, ranks)
-        idxes = compute_idxes_from_top_bot(start, end, partial_s_array, bwt, rank, first_occurrences)
-        pattern_indices[pattern] = idxes
-    return pattern_indices
+    seed_idxes = []
+    for i in range(0, len(read) - k + 1):
+        kmer = read[i:i+k]
+        start, end = bw_better_match_pattern(bwt, kmer, first_occurrences, checkpoint_arrs)
+        idxes = compute_idxes_from_top_bot(start, end, psa, bwt, ranks, first_occurrences)
+        seed_idxes.append(idxes)
+    return seed_idxes
 
 def main():
-    text = 'AATCGGGTTCAATCGGGGT'
-    patterns = ['ATCG', 'GGGT']
-    print(multiple_pattern_matching(text, patterns))
+    """Example string finder preprocessing and usage"""
+    ref = 'AATCGGGTTCAATCGGGGTAATCGGGTTCAATCGGGGT' # both ref and read will be passed in at another time
+    read = 'TCGGGTTCAATCGG'
+    # read2 = 'asdfasdfasdfas' # test chaff read
     
-    text = 'ATATATATAT'
-    patterns = ['GT', 'AGCT', 'TAA', 'AAT', 'AATAT']
-    print(multiple_pattern_matching(text, patterns))
+    K = 5 # partial suffix array constant
+    text = ref + '$'
+    bwt = burrows_wheeler_transform(text)
+    psa = partial_suffix_array(text, K)
+    first_occurrences = compute_first_occurrences(bwt)
+    checkpoint_arrs = compute_checkpoint_arrs(bwt)
+    ranks = compute_rank_arr(bwt)
+    seeds = generate_seeds(read, bwt, 8, psa, first_occurrences, checkpoint_arrs, ranks)
+    print(seeds)
     
-    text = 'bananas'
-    patterns = ['ana', 'as']
-    print(multiple_pattern_matching(text, patterns))
-    
-    text = 'AAACAA'
-    patterns = ['AA']
-    print(multiple_pattern_matching(text, patterns))
-
 if __name__ == '__main__':
     main()
