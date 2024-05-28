6338555f2d924525cf74144bc3687562423c868d
Adrian Layer <adrian.layer@gmail.com>
2024-05-27 19:12:32 -0700

    Refactored newSamFile.py to use max_seed v2 output

--- bwalign/newSamFile.py
+++ bwalign/newSamFile.py
@@ -1,6 +1,13 @@
 import pysam
 from Bio import SeqIO
 import numpy as np
+from compute_max_seed import compute_max_seed
+from generate_seeds import (burrows_wheeler_transform,
+                            partial_suffix_array,
+                            compute_first_occurrences,
+                            compute_checkpoint_arrs,
+                            compute_rank_arr,
+                            generate_seeds)
 
 def main():
     reference = SeqIO.read("reference.fasta", "fasta")
@@ -24,15 +31,8 @@ def main():
     #write to sam FIle
     with pysam.AlignmentFile("output.sam", "w", header=header) as samfile:
         for read in reads:
-            
             seed_idxes = generate_seeds(str(read.seq), bwt, 8, psa, first_occurrences, checkpoint_arrs, ranks)
-            
-            best_idx, _ = compute_max_seed(str(reference.seq), str(read.seq), seed_idxes, 2, -1, -2, -1)
-            
-            #AA
-            ref_segment = str(reference.seq)[best_idx:best_idx + 50]
-            score, alignment_s, alignment_t = AffineAlignment(2, -1, -2, -1, str(read.seq), ref_segment)
-            pos = alignment_t.find(alignment_t.replace('-', '')) + 1
+            best_idx, score, alignment_s, alignment_t = compute_max_seed(str(reference.seq), str(read.seq), seed_idxes, 2, -1, -2, -1)
 
             #create a SAM entry for the aligned read
             a = pysam.AlignedSegment()
@@ -40,11 +40,10 @@ def main():
             a.query_sequence = str(read.seq)
             a.flag = 0
             a.reference_id = 0
-            a.reference_start = pos - 1
+            a.reference_start = best_idx
             a.mapping_quality = 60
             a.cigarstring = f"{len(alignment_s.replace('-', ''))}M"
             a.query_qualities = pysam.qualitystring_to_array(read.letter_annotations["phred_quality"])
-
             
             samfile.write(a)
 
