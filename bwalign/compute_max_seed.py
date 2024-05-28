02ee600f4ab269dbae4e0bd53c546f4abc4cb333
Adrian Layer <adrian.layer@gmail.com>
2024-05-27 18:52:42 -0700

    Created v2 of max_seed.py

--- bwalign/compute_max_seed.py
+++ bwalign/compute_max_seed.py
@@ -2,25 +2,30 @@ from affine_alignment import AffineAlignment
 
 def compute_max_seed(ref: str, read: str, seed_idxes: list[list[int]],
                      match_reward: int, mismatch_penalty: int,
-                     gap_opening_penalty: int, gap_extension_penalty: int) -> tuple[int, int]:
+                     gap_opening_penalty: int, gap_extension_penalty: int) -> tuple[int, int, str, str]:
     """
     For each index in the seeds list, generates an affine alignment (50x50) for each seed index.
     By the end, returns position in ref and score of max scoring alignment. The alignment
     will be between the entire read and a length 50 segment of the reference starting at
-    the calculated position.
+    the calculated position. Version 2 of this function now also returns the alignments 
+    themselves of the read and respective 50 base segment of the ref genome.
     """
     best_score = float('-inf')
     best_idx = -1
+    s_best_align = ''
+    t_best_align = ''
     for i in range(0, len(seed_idxes)):
         for ref_idx in seed_idxes[i]:
             ref_segment = ref[ref_idx - i:ref_idx - i + 50]
-            score, _, _, = AffineAlignment(match_reward, mismatch_penalty, 
+            score, s_align, t_align, = AffineAlignment(match_reward, mismatch_penalty, 
                                             gap_opening_penalty, gap_extension_penalty,
                                             ref_segment, read)
             if score > best_score:
                 best_score = score
                 best_idx = ref_idx
-    return best_idx, best_score
+                s_best_align = s_align
+                t_best_align = t_align
+    return best_idx, best_score, s_best_align, t_best_align
 
 def main():
     # still need some more test cases for this to be sure it works as intended.
